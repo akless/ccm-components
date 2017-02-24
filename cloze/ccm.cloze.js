9eaf7461e850af202f9e8020a27964f9aa9c73d8
Andre Kless <andre.kless@inf.h-brs.de>
2017-02-24 18:35:05 +0100

    update component for clozes

--- cloze/ccm.cloze.js
+++ cloze/ccm.cloze.js
@@ -14,15 +14,47 @@ ccm.component( {
   name: 'cloze',
 
   config: {
-  //blank: true
-  //button: 'submit',
-    data: {
-      store: [ ccm.store, '../cloze/datastore.json' ],
-      key: 'default'
+
+    text: 'Hello, [[(W)o(rl)d]]!',
+    html_templates: {
+      "main": {
+        "key": "main",
+        "class": "main",
+        "inner": [
+          { "class": "keywords" },
+          { "class": "text" },
+          { "class": "button" },
+          { "class": "timer" }
+        ]
+      },
+      "keyword": {
+        "class": "keyword",
+        "inner": "%%"
+      },
+      "button": {
+        "key": "button",
+        "tag": "button",
+        "class": "%type%",
+        "inner": "%caption%",
+        "onclick": "%click%"
+      },
+      "timer": {
+        "key": "timer",
+        "tag": "span",
+        "inner": "%%"
+      }
     },
-    html:  [ ccm.store, '../cloze/templates.json' ],
-  //keywords: [ 'keyword1', 'keyword2', ... ]
-    style: [ ccm.load, '../cloze/layout.css' ]
+    css_layout: [ ccm.load, '../cloze/layout.css' ],
+    submit_button_caption: 'submit',
+    finish_button_caption: 'restart',
+    onFinish: function ( instance, result ) { console.log( result ); instance.render() }
+
+//  blank: true,
+//  keywords: [ 'keyword1', 'keyword2', ... ],
+//  points_per_gap: 1,
+//  time: 60,
+//  user: [ ccm.instance, '../user/ccm.user.js' ]
+
   },
 
   Instance: function () {
@@ -30,7 +62,6 @@ ccm.component( {
     var self = this;
     var my;
 
-    var dataset;        // dataset for rendering
     var keywords = [];  // information data for each keyword
 
     this.init = function ( callback ) {
@@ -43,88 +74,90 @@ ccm.component( {
 
     this.ready = function ( callback ) {
 
-      // get dataset for rendering
-      ccm.helper.dataset( my.data, function ( result ) {
-        dataset = result;
+      // fill-in-the-blank text is given via inner HTML? => use it with higher priority
+      if ( self.node.innerHTML.trim() ) my.text = self.node.innerHTML;
 
-        // determine text with containing gaps
-        if ( dataset ) my.text = dataset.text;
-        if ( self.node.innerHTML.trim() ) my.text = self.node.innerHTML;
+      var regex_keyword = /\[\[.+?\]\]/g;   // regular expression for finding all gaps/keywords in the text
+      var regex_given = /\(.+?\)/g;         // regular expression for finding all given characters of a keyword
 
-        var regex_keyword = /\[\[.+?\]\]/g;   // regular expression for finding all gaps/keywords in the text
-        var regex_given = /\(.+?\)/g;         // regular expression for finding all given characters of a keyword
+      // iterate all keywords in the text
+      ( my.text.match( regex_keyword ) || [] ).map( function ( keyword ) {
 
-        // iterate all keywords in the text
-        my.text.match( regex_keyword ).map( function ( keyword ) {
+        // remove distinguishing characteristic '[[' and ']]'
+        keyword = keyword.substr( 2, keyword.length - 4 );
 
-          // remove distinguishing characteristic '[[' and ']]'
-          keyword = keyword.substr( 2, keyword.length - 4 );
-
-          // replace all given characters of a keywords with '*'
-          var keyw__d = keyword.replace( '*', '#' ).replace( regex_given, function ( given ) {
-            var length = given.length - 2;
-            given = '';
-            for ( var i = 0; i < length; i++ ) given += '*';
-            return given;
-          } );
-
-          // determine given characters and hold this information in a single number (use of bit operations)
-          var givens = 0;
-          for ( var i = 0; i < keyw__d.length; i++ )
-            if ( keyw__d.charAt( i ) === '*' ) givens += Math.pow( 2, i );
-
-          // add keyword information data for current keyword
-          keywords.push( {
-            word: keyword.replace( regex_given, function ( given ) { return given.substr( 1, given.length - 2 ); } ),
-            givens: givens
-          } );
-
+        // replace all given characters of a keywords with '*'
+        var keyw__d = keyword.replace( '*', '#' ).replace( regex_given, function ( given ) {
+          var length = given.length - 2;
+          given = '';
+          for ( var i = 0; i < length; i++ ) given += '*';
+          return given;
         } );
 
-        // replace gaps/keywords with empty span elements
-        my.text = my.text.replace( regex_keyword, '<span class="cloze_gap"></span>' );
+        // determine given characters and hold this information in a single number (use of bit operations)
+        var givens = 0;
+        for ( var i = 0; i < keyw__d.length; i++ )
+          if ( keyw__d.charAt( i ) === '*' ) givens += Math.pow( 2, i );
 
-        // preload HTML templates
-        my.html.get( callback );
+        // add keyword information data for current keyword
+        keywords.push( {
+          word: keyword.replace( regex_given, function ( given ) { return given.substr( 1, given.length - 2 ); } ),
+          givens: givens
+        } );
 
       } );
 
+      // replace gaps/keywords with empty span elements
+      my.text = my.text.replace( regex_keyword, '<span class="gap"></span>' );
+
+      callback();
     };
 
     this.render = function ( callback ) {
 
-      // render main HTML structure
-      self.element.innerHTML = '';
-      self.element.appendChild( ccm.helper.html( my.html.get( 'main' ) ) );
-      var elem_main = self.element.querySelector( '.main' );
+      // initial result data
+      var result = { points: 0, details: [] };
 
+      // render main HTML structure
+      ccm.helper.setContent( self.element, ccm.helper.protect( ccm.helper.html( my.html_templates.main ) ) );
+
+      var   main_elem  = self.element.querySelector( '.main'   );  // main container
+      var button_elem  = self.element.querySelector( '.button' );  // container for submit button
+      var  timer_elem  = self.element.querySelector( '.timer'  );  // container for timer
+      var  timer_value = my.time;                                  // timer value
+
+      // render content for inner containers
       renderKeywords();
       renderText();
-      renderButton();
+      renderButton( true );
+      if ( my.time ) renderTimer(); else ccm.helper.removeElement( timer_elem );
+
+      // remember start time
+      var time = new Date().getTime();
 
       if ( callback ) callback();
 
       /** renders given keywords for text gaps */
       function renderKeywords() {
 
-        var elem_keywords = self.element.querySelector( '.keywords' );  // container for keywords
+        var keywords_elem = self.element.querySelector( '.keywords' );  // container for keywords
         var entries = [];                                               // inner container for each keyword
 
         // has given keywords? => create inner container for each keyword
         if ( my.keywords )
           ( Array.isArray( my.keywords ) ? my.keywords : keywords ).map( addKeyword );
         else
-          return elem_main.removeChild( elem_keywords );  // no given keywords? => remove container for keywords
+          return main_elem.removeChild( keywords_elem );  // no given keywords? => remove container for keywords
 
         // generated keyword list? => sort keywords lexicographical (keyword order gives no hint about correct solution)
         if ( my.keywords === true ) entries.sort( function ( a, b ) { return a.innerHTML.localeCompare( b.innerHTML ) } );
 
         // add each inner keyword container to container for keywords
-        entries.map( function ( entry ) { elem_keywords.appendChild( entry ); } );
+        entries.map( function ( entry ) { keywords_elem.appendChild( ccm.helper.protect( entry ) ); } );
 
         /** adds a inner container for a keyword */
         function addKeyword( keyword ) {
-          entries.push( ccm.helper.html( { class: 'keyword', inner: Array.isArray( my.keywords ) ? keyword : keyword.word } ) );
+          entries.push( ccm.helper.html( my.html_templates.keyword, Array.isArray( my.keywords ) ? keyword : keyword.word ) );
         }
 
       }
@@ -133,13 +166,13 @@ ccm.component( {
       function renderText() {
 
         // render text with containing gaps
-        self.element.querySelector( '.text' ).innerHTML = my.text;
+        self.element.querySelector( '.text' ).innerHTML = ccm.helper.protect( my.text );
 
         // render input field in each gap
-        ccm.helper.makeIterable( self.element.querySelectorAll( '.cloze_gap' ) ).map( function ( gap, i ) {
+        ccm.helper.makeIterable( self.element.querySelectorAll( '.gap' ) ).map( function ( gap_elem, i ) {
 
           // blank input fields and shown keywords? => input fields should give no hint for the length of the searched word
-          if ( my.blank && my.keywords ) return gap.appendChild( ccm.helper.html( { tag: 'input', type: 'text' } ) );
+          if ( my.blank && my.keywords ) return gap_elem.appendChild( ccm.helper.html( { tag: 'input', type: 'text' } ) );
 
           // shorter access to keyword
           var keyword = keywords[ i ].word;
@@ -160,34 +193,97 @@ ccm.component( {
           }
 
           // render input field in the current gap
-          gap.appendChild( ccm.helper.html( input ) );
+          gap_elem.appendChild( ccm.helper.html( input ) );
 
         } );
 
       }
 
-      /** renders the submit button */
-      function renderButton() {
+      /**
+       * renders the submit or finish button
+       * @param {boolean} type - true: submit button, false: finish button
+       */
+      function renderButton( type ) {
 
-        self.element.querySelector( '.button' ).appendChild( ccm.helper.html( my.html.get( 'button' ), {
-          button: my.button || dataset.button,
-          click: submit
-        } ) );
+        ccm.helper.setContent( button_elem, ccm.helper.protect( ccm.helper.html( my.html_templates.button, {
+          caption: type ? my.submit_button_caption : my.finish_button_caption,
+          type:    type ? 'submit' : 'finish',
+          click:   type ? onSubmit : onFinish
+        } ) ) );
 
-        /** onclick callback for the submit button */
-        function submit() {
+      }
 
-          // give visual feedback for correctness
-          ccm.helper.makeIterable( self.element.querySelectorAll( '.cloze_gap input' ) ).map( function ( gap, i ) {
-            gap.removeAttribute( 'placeholder' );
-            gap.style.backgroundColor = gap.value === keywords[ i ].word ? 'lime' : 'red';
-          } );
+      /** renders the timer */
+      function renderTimer() {
+
+        timer();  // start timer
+
+        /** updates countdown timer */
+        function timer() {
+
+          // already submitted? => remove timer
+          if ( !self.element.querySelector( '.timer' ) ) return;
+
+          // (re)render timer value
+          ccm.helper.setContent( timer_elem, ccm.helper.protect( ccm.helper.html( my.html_templates.timer, timer_value ) ) );
+
+          // countdown
+          if ( timer_value-- )
+            ccm.helper.wait( 1000, timer );
+          else if ( button_elem )
+            onSubmit();           // submit at timeout
 
         }
 
       }
 
-    };
+      /** onclick callback for submit button */
+      function onSubmit() {
+
+        time = new Date().getTime() - time;      // calculate result time
+        ccm.helper.removeElement( timer_elem );  // remove timer
+
+        // give visual feedback for correctness
+        ccm.helper.makeIterable( self.element.querySelectorAll( '.gap input' ) ).map( function ( gap, i ) {
+          var correct = gap.value === keywords[ i ].word;
+          gap.removeAttribute( 'placeholder' );
+          gap.parentNode.className += correct ? ' correct' : ' wrong';
+
+          // correct input? => give points
+          if ( correct && my.points_per_gap ) result.points += my.points_per_gap;
+
+          // set details for current gap result
+          result.details.push( { input: gap.value, solution: keywords[ i ].word, correct: correct } );
+
+        } );
+
+        // replace submit button with finish button
+        renderButton( false );
+
+      }
+
+      /** onclick callback for finish button */
+      function onFinish() {
+
+        // has user instance? => login user
+        if ( self.user ) self.user.login( proceed ); else proceed();
+
+        function proceed() {
+
+          // finalize result data
+          if ( !my.points_per_gap ) delete result.points;
+          if ( self.user ) result.user = self.user.data().key;
+          if ( my.time )   result.time_left = timer_value + 1;
+          result.time = time;
+
+          // perform finish callback with result data
+          self.onFinish( self, result );
+
+        }
+
+      }
+
+    }
 
   }
 
