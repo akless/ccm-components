1841771836df74420edd2927481ae475d9a8e566
Andre Kless <andre.kless@inf.h-brs.de>
2017-02-26 19:36:08 +0100

    add component for user authentication

---
+++ user/ccm.user.js
@@ -0,0 +1,291 @@
+/**
+ * @overview <i>ccm</i> component for user authentication
+ * @author André Kless <andre.kless@web.de> 2015-2017
+ * @license The MIT License (MIT)
+ */
+
+ccm.component( {
+
+  name: 'user',
+
+  config: {
+
+    html_templates: {
+      logged_in: {
+        id: 'logged_in',
+        inner: [
+          {
+            id: 'username',
+            inner: '%user%',
+            title: '%user_title%'
+          },
+          {
+            id: 'button',
+            inner: {
+              tag: 'button',
+              inner: '%logout%',
+              title: '%logout_title%',
+              onclick: '%click%'
+            }
+          }
+        ]
+      },
+      logged_out: {
+        id: 'logged_out',
+        inner: {
+          id: 'button',
+          inner: {
+            tag: 'button',
+            inner: '%login%',
+            title: '%title%',
+            onclick: '%click%'
+          }
+        }
+      }
+    },
+    css_layout: [ ccm.load, '../user/layouts/default.css' ],
+    ccm_helper: [ ccm.load, '../../ccm-developer-no-jquery/ccm/ccm-helper.js' ],
+    context: true,
+    logged_in: false,
+    sign_on: 'guest',
+    texts: {
+      login:        'Login',
+      login_title:  'click here for login',
+      logout:       'Logout',
+      logout_title: 'click here for logout',
+      user_title:   'this is your username'
+    },
+    guest: {
+      key:   'guest',
+      name:  'Guest User'
+    }
+
+  },
+
+  Instance: function () {
+
+    var self = this;
+
+    /**
+     * privatized instance members
+     * @type {object}
+     */
+    var my;
+
+    /**
+     * dataset of the current logged in user
+     * @type {ccm.components.user.types.dataset}
+     */
+    var dataset = null;
+
+    /**
+     * observers for login and logout event
+     * @type {function[]}
+     */
+    var observers = [];
+
+    /**
+     * true during a login or logout request
+     * @type {boolean}
+     */
+    var loading = false;
+
+    /**
+     * @summary waitlist during a login or logout request
+     * @description Waitlist of actions that must be performed after a successful login or logout request.
+     * @type {ccm.types.action[]}
+     */
+    var waitlist = [];
+
+    this.init = function ( callback ) {
+
+      // context mode? => set context to highest ccm instance for user authentication in current ccm context
+      if ( self.context ) { var context = ccm.context.find( self, 'user' ); self.context = context && context.context || context || false; }
+
+      callback();
+    };
+
+    this.ready = function ( callback ) {
+
+      // privatize all possible instance members
+      my = ccm.helper.privatize( self );
+
+      // immediate login? => login user
+      if ( my.logged_in ) self.login( callback ); else callback();
+
+    };
+
+    this.render = function ( callback ) {
+
+      // context mode? => delegate function call
+      if ( my.context ) return my.context.render( callback );
+
+      // prepare main HTML structure
+      var main_elem = self.isLoggedIn() ? ccm.helper.html( my.html_templates.logged_in, {
+        user:           self.data().key,
+        user_title: my.texts.user_title,
+        logout:         my.texts.logout,
+        logout_title:   my.texts.logout_title,
+        click:          function () { self.logout( self.render ); }
+      } ) : ccm.helper.html( my.html_templates.logged_out, {
+        login: my.texts.login,
+        title: my.texts.login_title,
+        click: function () { self.login( self.render ); }
+      } );
+
+      // set content of own website area
+      ccm.helper.setContent( self.element, ccm.helper.protect( main_elem ) );
+
+      if ( callback ) callback();
+    };
+
+    /**
+     * login user
+     * @param {function} [callback] will be called after login (or directly if user is already logged in)
+     */
+    this.login = function ( callback ) {
+
+      // context mode? => delegate function call
+      if ( my.context ) return my.context.login( callback );
+
+      // user already logged in? => perform callback directly
+      if ( self.isLoggedIn() ) { if ( callback ) callback(); return; }
+
+      // prevent more than one request on parallel login/logout calls
+      if ( loading ) return waitlist.push( [ self.login, callback ] ); loading = true;
+
+      // choose sign on and proceed login
+      switch ( my.sign_on ) {
+        case 'guest':
+          success( my.guest.key, '', my.guest.name );
+          break;
+        case 'demo':
+          ccm.load( [ 'https://kaul.inf.h-brs.de/login/demo_login.php', { realm: 'hbrsinfkaul' } ], function ( response ) { success( response.user, response.token ); } );
+          break;
+        case 'hbrsinfkaul':
+          ccm.load( [ 'https://kaul.inf.h-brs.de/login/login.php', { realm: 'hbrsinfkaul' } ], function ( response ) { success( response.user, response.token, response.name ); } );
+          break;
+      }
+
+      /**
+       * callback when login was successful
+       * @param {string} key - dataset key and username, respectively
+       * @param {string} token - security token
+       * @param {string} name - full name of user
+       */
+      function success( key, token, name ) {
+
+        dataset = { key: key, token: token, name: name };  // hold user data
+        loading = false;                                   // request is finished
+
+        // perform waiting functions
+        while ( waitlist.length > 0 ) ccm.helper.action( waitlist.shift() );
+
+        if ( self.element ) self.render();  // (re)render own content
+        if ( callback ) callback();         // perform callback
+        notify( true );                     // notify observers about login event
+
+      }
+
+    };
+
+    /**
+     * logout user
+     * @param {function} [callback] will be called after logout (or directly if user is already logged out)
+     */
+    this.logout = function ( callback ) {
+
+      // context mode? => delegate function call
+      if ( my.context ) return my.context.logout( callback );
+
+      // user already logged out? => perform callback directly
+      if ( !self.isLoggedIn() ) { if ( callback ) callback(); return; }
+
+      // prevent more than one request on parallel login/logout calls
+      if ( loading ) return waitlist.push( [ self.logout, callback ] ); loading = true;
+
+      // choose sign on and proceed logout
+      switch ( my.sign_on ) {
+        case 'guest':
+          success();
+          break;
+        case 'demo':
+          ccm.load( [ 'https://logout@kaul.inf.h-brs.de/login/demo_logout.php', { realm: 'hbrsinfkaul' } ], success );
+          break;
+        case 'hbrsinfkaul':
+          ccm.load( [ 'https://logout@kaul.inf.h-brs.de/login/logout.php', { realm: 'hbrsinfkaul' } ], success );
+          break;
+      }
+
+      /** callback when logout was successful */
+      function success() {
+
+        dataset = null;   // forget user data
+        loading = false;  // request is finished
+
+        // perform waiting functions
+        while ( waitlist.length > 0 ) ccm.helper.action( waitlist.shift() );
+
+        if ( self.element ) self.render();  // (re)render own content
+        if ( callback ) callback();         // perform callback
+        notify( false );                    // notify observers about logout event
+
+      }
+
+    };
+
+    /**
+     * checks if user is logged in
+     * @returns {boolean}
+     */
+    this.isLoggedIn = function () {
+
+      // context mode? => delegate function call
+      if ( my.context ) return my.context.isLoggedIn();
+
+      // user is logged in if user data exists
+      return !!dataset;
+
+    };
+
+    /**
+     * returns user dataset
+     * @returns {object}
+     */
+    this.data = function () {
+
+      // context mode? => delegate function call
+      if ( my.context ) return my.context.data();
+
+      return dataset;
+    };
+
+    /**
+     * adds an observer for login and logout events
+     * @param {function} observer - will be performed when event fires (first parameter is kind of event -> true: login, false: logout)
+     */
+    this.addObserver = function ( observer ) {
+
+      // context mode? => delegate function call
+      if ( my.context ) return my.context.addObserver( observer );
+
+      // add function to observers
+      observers.push( observer );
+
+    };
+
+    /**
+     * notify observers
+     * @param {boolean} event - true: login, false: logout
+     * @private
+     */
+    function notify( event ) {
+
+      // perform observer functions
+      observers.map( function ( observer ) { observer( event ); } );
+
+    }
+
+  }
+
+} );
