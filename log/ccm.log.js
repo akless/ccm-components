1c5be5240e0d2ae82ca8dafa55bc804350118715
Andre Kless <andre.kless@inf.h-brs.de>
2017-03-30 11:19:28 +0200

    update ccm component for data logging

--- log/ccm.log.js
+++ log/ccm.log.js
@@ -4,105 +4,116 @@
  * @license The MIT License (MIT)
  */
 
-ccm.component( {
+( function () {
 
-  name: 'log',
+  var ccm_version = '8.0.0';
+  var ccm_url     = '../../ccm-developer-no-jquery/ccm/ccm.js';
 
-  config: {
+  var component_name = 'log';
+  var component_obj  = {
 
-    hash:     [ ccm.load, '../libs/md5/md5.min.js' ],
-    logging:  {},
-    onfinish: function ( instance, results ) { console.log( results ); }
+    name: component_name,
 
-    // events: {string[]} logged events, default: all
-    // logging.data:    {boolean|string[]} log event specific informations
-    // logging.browser: {boolean|string[]} log browser informations
-    // logging.parent:  {boolean|string[]} log ccm context parent information
-    // logging.root:    {boolean|string[]} log ccm context root information
-    // logging.user:    {boolean|string[]} log user informations
-    // logging.website: {boolean|string[]} log website informations (string[] -> log informations only for these events)
+    config: {
 
-  },
+      hash:     [ 'ccm.load', '../libs/md5/md5.min.js' ],
+      logging:  {},
+      onfinish: function ( instance, results ) { console.log( results ); }
 
-  Instance: function () {
+      // events: {string[]} logged events, default: all
+      // logging.data:    {boolean|string[]} log event specific informations
+      // logging.browser: {boolean|string[]} log browser informations
+      // logging.parent:  {boolean|string[]} log ccm context parent information
+      // logging.root:    {boolean|string[]} log ccm context root information
+      // logging.user:    {boolean|string[]} log user informations
+      // logging.website: {boolean|string[]} log website informations (string[] -> log informations only for these events)
 
-    var self = this;
-    var my;           // contains privatized instance members
-    var id;           // global unique id of this instance
+    },
 
-    this.ready = function ( callback ) {
+    Instance: function () {
 
-      // privatize all possible instance members
-      my = ccm.helper.privatize( self );
+      var self = this;
+      var my;           // contains privatized instance members
+      var id;           // global unique id of this instance
 
-      // determine global unique instance id
-      id = ccm.helper.generateKey();
+      this.ready = function ( callback ) {
 
-      callback();
-    };
+        // privatize all possible instance members
+        my = self.ccm.helper.privatize( self );
 
-    /**
-     * logs event data
-     * @param {string} event - unique event index
-     * @param {object} [data] - event specific informations
-     */
-    this.log = function ( event, data ) {
+        // determine global unique instance id
+        id = self.ccm.helper.generateKey();
 
-      // ignored event? => abort
-      if ( my.events && !my.events[ event ] ) return;
+        callback();
+      };
 
       /**
-       * result data
-       * @type {object}
+       * logs event data
+       * @param {string} event - unique event index
+       * @param {object} [data] - event specific informations
        */
-      var results = { instance: id, event: event };
+      this.log = function ( event, data ) {
 
-      // add event specific informations
-      if ( data !== undefined && ( ccm.helper.isObject( my.logging.data ) ? my.logging.data[ event ] : my.logging.data ) ) results.data = ccm.helper.clone( data );
+        // ignored event? => abort
+        if ( my.events && !my.events[ event ] ) return;
 
-      // add browser informations
-      if ( ccm.helper.isObject( my.logging.browser ) ? my.logging.browser[ event ] : my.logging.browser ) results.browser = {
-        appCodeName: navigator.appCodeName,
-        appName: navigator.appName,
-        appVersion: navigator.appVersion,
-        language: navigator.language,
-        oscpu: navigator.oscpu,
-        platform: navigator.platform,
-        userAgent: navigator.userAgent
+        /**
+         * result data
+         * @type {object}
+         */
+        var results = { instance: id, event: event };
+
+        // add event specific informations
+        if ( data !== undefined && ( self.ccm.helper.isObject( my.logging.data ) ? my.logging.data[ event ] : my.logging.data ) ) results.data = self.ccm.helper.clone( data );
+
+        // add browser informations
+        if ( self.ccm.helper.isObject( my.logging.browser ) ? my.logging.browser[ event ] : my.logging.browser ) results.browser = {
+          appCodeName: navigator.appCodeName,
+          appName: navigator.appName,
+          appVersion: navigator.appVersion,
+          language: navigator.language,
+          oscpu: navigator.oscpu,
+          platform: navigator.platform,
+          userAgent: navigator.userAgent
+        };
+
+        // add ccm context parent informations
+        if ( self.parent && ( self.ccm.helper.isObject( my.logging.parent ) ? my.logging.parent[ event ] : my.logging.parent ) ) results.parent = {
+          name:    self.parent.component.name,
+          version: self.parent.component.version
+        };
+
+        // add ccm context root informations
+        if ( self.parent && ( self.ccm.helper.isObject( my.logging.root ) ? my.logging.root[ event ] : my.logging.root ) ) {
+          var root = self.ccm.context.root( self );
+          results.root = {
+            name:    root.component.name,
+            version: root.component.version
+          };
+        }
+
+        // add user informations
+        if ( self.ccm.helper.isObject( my.logging.user ) ? my.logging.user[ event ] : my.logging.user ) {
+          var user = self.ccm.context.find( self, 'user' );
+          if ( user ) results.user = {
+            key:     user.isLoggedIn() ? ( md5 ? md5( md5( user.data().key ) ) : user.data().key ) : null,
+            sign_on: user.getSignOn()
+          };
+        }
+
+        // add website informations
+        if ( self.ccm.helper.isObject( my.logging.website ) ? my.logging.website[ event ] : my.logging.website ) results.website = window.location.href;
+
+        // provide result data
+        self.ccm.helper.onFinish( self, results );
+
       };
 
-      // add ccm context parent informations
-      if ( self.parent && ( ccm.helper.isObject( my.logging.parent ) ? my.logging.parent[ event ] : my.logging.parent ) ) results.parent = {
-        name:    self.parent.component.name,
-        version: self.parent.component.version
-      };
+    }
 
-      // add ccm context root informations
-      if ( self.parent && ( ccm.helper.isObject( my.logging.root ) ? my.logging.root[ event ] : my.logging.root ) ) {
-        var root = ccm.context.root( self );
-        results.root = {
-          name:    root.component.name,
-          version: root.component.version
-        };
-      }
+  };
 
-      // add user informations
-      if ( ccm.helper.isObject( my.logging.user ) ? my.logging.user[ event ] : my.logging.user ) {
-        var user = ccm.context.find( self, 'user' );
-        if ( user ) results.user = {
-          key:     user.isLoggedIn() ? ( md5 ? md5( md5( user.data().key ) ) : user.data().key ) : null,
-          sign_on: user.getSignOn()
-        };
-      }
-
-      // add website informations
-      if ( ccm.helper.isObject( my.logging.website ) ? my.logging.website[ event ] : my.logging.website ) results.website = window.location.href;
-
-      // provide result data
-      ccm.helper.onFinish( self, results );
-
-    };
-
-  }
-
-} );
+  var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
+  if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register();
+  function register() { ccm[ ccm_version ].component( component_obj ); }
+}() );
