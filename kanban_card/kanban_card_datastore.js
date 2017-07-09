14c6eabf6d102dc324a9bacb20681e8978b5905b
Andre Kless <andre.kless@inf.h-brs.de>
2017-07-09 21:58:45 +0200

    add meta data in kanban card resources

--- kanban_card/kanban_card_datastore.js
+++ kanban_card/kanban_card_datastore.js
@@ -1,3 +1,9 @@
+/**
+ * @overview <i>ccm</i> datasets of ccm component for rendering a kanban card
+ * @author André Kless <andre.kless@web.de> 2016-2017
+ * @license The MIT License (MIT)
+ */
+
 ccm.files[ "kanban_card_datastore.min.js" ] = {
   "homework": {
     "title": "Homework",
