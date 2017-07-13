5e52b3b698c00371f5616bcba3cddab67547014f
Andre Kless <andre.kless@inf.h-brs.de>
2017-07-13 17:02:05 +0200

    update ccm path in cloze component

--- cloze/ccm.cloze.js
+++ cloze/ccm.cloze.js
@@ -7,7 +7,7 @@
 ( function () {
 
   var ccm_version = '9.0.0';
-  var ccm_url     = 'https://akless.github.io/ccm/version/ccm-9.0.0.min.js';
+  var ccm_url     = 'https://akless.github.io/ccm/ccm.min.js';
 
   var component_name = 'cloze';
   var component_obj  = {
