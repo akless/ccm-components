2bd3397005f1764546b4ef3065f6477fa37ccfc2
Andre Kless <andre.kless@inf.h-brs.de>
2017-07-14 20:38:41 +0200

    add component for redering component informations

---
+++ comp_info/resources/comp_info_configs.js
@@ -0,0 +1,14 @@
+/**
+ * @overview configurations of <i>ccm</i> component for rendering component informations
+ * @author André Kless <andre.kless@web.de> 2017
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ "comp_info_configs.min.js" ] = {
+  "cloze": {
+    "data": {
+      "store": [ "ccm.store", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_datasets.min.js" ],
+      "key": "cloze"
+    }
+  }
+};
---
+++ comp_info/resources/comp_info_datasets.js
@@ -0,0 +1,19 @@
+/**
+ * @overview datasets of <i>ccm</i> component for rendering component informations
+ * @author André Kless <andre.kless@web.de> 2017
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ "comp_info_datasets.min.js" ] = {
+  "cloze": {
+    "logo": "https://akless.github.io/ccm-components/component.png",
+    "title": "Fill-in-the-Blank Text",
+    "name": "cloze",
+    "abstract": "<i>ccm</i> component for rendering a fill-in-the-blank text.",
+    "url": "https://akless.github.io/ccm-components/cloze/ccm.cloze.js",
+    "developer": "André Kless",
+    "licence": "The MIT License (MIT)",
+    "website": "https://akless.github.io/ccm-components/cloze/",
+    "demo": [ "ccm.get", "https://akless.github.io/ccm-components/cloze/resources/cloze_configs.min.js", "demo" ]
+  }
+};
