91993410bed7deddac9cfa4255d795095f7f7b75
Andre Kless <andre.kless@inf.h-brs.de>
2017-07-16 02:21:58 +0200

    update le component

--- le/ccm.le.js
+++ le/ccm.le.js
@@ -119,7 +119,10 @@
         // add footer
         if ( my.author ) {
           var footer = document.createElement( 'footer' );
-          footer.innerHTML = '<hr><p xmlns:dct="https://purl.org/dc/terms/"><a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/"><img src="https://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0"></a><br>Soweit unter den gesetzlichen Voraussetzungen möglich hat <span resource="[_:publisher]" rel="dct:publisher"><span property="dct:title">' + my.author + '</span></span> sämtliche Urheber- und Verwertungsrechte für dieses Werk abgetreten.</p>';
+          if ( my.english_licence )
+            footer.innerHTML = '<hr><p xmlns:dct="https://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#"> <a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/"> <img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0"></a><br>To the extent possible under law, <span resource="[_:publisher]" rel="dct:publisher"><span property="dct:title">' + my.author + '</span></span> has waived all copyright and related or neighboring rights to this work. This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" content="DE" about="[_:publisher]">Deutschland</span>.</p>';
+          else
+            footer.innerHTML = '<hr><p xmlns:dct="https://purl.org/dc/terms/"><a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/"><img src="https://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0"></a><br>Soweit unter den gesetzlichen Voraussetzungen möglich hat <span resource="[_:publisher]" rel="dct:publisher"><span property="dct:title">' + my.author + '</span></span> sämtliche Urheber- und Verwertungsrechte für dieses Werk abgetreten.</p>';
           my.node.appendChild( footer );
           delete my.author;
         }
