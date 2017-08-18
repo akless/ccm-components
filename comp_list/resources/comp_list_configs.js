56c6050bf071da76b12c62a6071ba742b6ee1357
Andre Kless <andre.kless@inf.h-brs.de>
2017-08-18 13:43:27 +0200

    move ccm.comp_info.js to ccm-components-not-ready branch

---
+++ comp_list/resources/comp_list_configs.js
@@ -0,0 +1,27 @@
+/**
+ * @overview configurations of <i>ccm</i> component for rendering a component list
+ * @author André Kless <andre.kless@web.de> 2017
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ "comp_list_configs.min.js" ] = {
+  "demo": {
+    "comp_info_configs": [
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "cloze" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "fill_in_the_blank_text_builder" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "quizz" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "kanban_board" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "kanban_card" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "editor" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "forum" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "posts" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "question" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "slidecast" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "star_rating" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "star_rating_result" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "thumb_rating" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "voting" ],
+      [ "ccm.get", "https://akless.github.io/ccm-components/comp_info/resources/comp_info_configs.min.js", "desktop" ]
+    ]
+  }
+};
