c6141a612df608ff23d40d53ec6b9a064287e306
Andre Kless <andre.kless@inf.h-brs.de>
2017-08-22 12:53:22 +0200

    update ccm.content.js to latest framework version

---
+++ content/resources/configs.js
@@ -0,0 +1,11 @@
+/**
+ * @overview configurations of ccm component for rendering a fill-in-the blank text
+ * @author André Kless <andre.kless@web.de> 2017
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ 'configs.js' ] = {
+  "demo": {
+    "inner": "<script src=../blank/ccm.blank.js></script>Welcome.<ccm-blank></ccm-blank>"
+  }
+};
--- content/versions/ccm.content-2.0.0.js
+++ content/versions/ccm.content-2.0.0.js
@@ -1,11 +1,12 @@
 /**
- * @overview <i>ccm</i> component for composed content
+ * @overview ccm component for rendering a predefined content
  * @author André Kless <andre.kless@web.de> 2016-2017
  * @license The MIT License (MIT)
  * @version 2.0.0
  * @changes
- * version 2.0.0 (01.08.2017):
- * - uses ccm v9.0.0 instead of v8.1.0
+ * version 2.0.0 (22.08.2017):
+ * - uses ccm v10.0.0 instead of v8.1.0
+ * - shortened component backbone
  * - use fragment instead of empty container as default Light DOM
  * - Light DOM can be given as HTML string via 'inner' config property
  * - removed no more needed ccm.helper.protect calls
@@ -14,16 +15,16 @@
 
 ( function () {
 
-  var filename = 'ccm.content-2.0.0.min.js';
+  var component = {
 
-  var ccm_version = '9.0.0';
-  var ccm_url     = 'https://akless.github.io/ccm/version/ccm-9.0.0.min.js';
-
-  var component_name = 'content';
-  var component_obj  = {
-
-    name: component_name,
+    name: 'content',
     version: [ 2, 0, 0 ],
+
+    ccm: {
+      url: 'https://akless.github.io/ccm/version/ccm-10.0.0.min.js',
+      integrity: 'sha384-6j2e9RlJGOcd5Ge57Z1lzJSV1t4JrlPnPXZ9nZQ9vG1w0rXFhrMeiWx1EVySMM3Z',
+      crossorigin: 'anonymous'
+    },
 
     Instance: function () {
 
@@ -119,8 +120,5 @@
 
   };
 
-  if ( window.ccm && window.ccm.files ) window.ccm.files[ filename ] = component_obj;
-  var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
-  if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register( true );
-  function register( synchron ) { ccm[ ccm_version ].component( component_obj ); if ( !synchron ) delete window.ccm.files[ filename ]; }
+  function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
 }() );
