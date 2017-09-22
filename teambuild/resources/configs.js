af70276b931dde7c13d25a7917263a0401ccb7e5
Andre Kless <andre.kless@inf.h-brs.de>
2017-09-22 10:05:15 +0200

    move component for realtime team building from 'not-ready' to 'master' branch

--- teambuild/resources/configs.js
+++
@@ -1,9 +0,0 @@
-/**
- * @overview configurations of ccm component for realtime team building
- * @author André Kless <andre.kless@web.de> 2017
- * @license The MIT License (MIT)
- */
-
-ccm.files[ 'configs.js' ] = {
-  "local": {}
-};
