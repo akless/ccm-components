ac0026c8e0eed3867f9d888eca1492b4ed4c684f
Andre Kless <andre.kless@inf.h-brs.de>
2017-09-26 08:37:33 +0200

    load Font Awesome first in head an then in Shadow DOM context

--- kanban_card/ccm.kanban_card.js
+++ kanban_card/ccm.kanban_card.js
@@ -105,12 +105,12 @@
       "icons": [
         "ccm.load",
         {
+          "context": "head",
           "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css",
           "integrity": "sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+",
           "crossorigin": "anonymous"
         },
         {
-          "context": "head",
           "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css",
           "integrity": "sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+",
           "crossorigin": "anonymous"
