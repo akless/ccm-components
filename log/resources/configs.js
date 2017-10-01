8563fff01b84c15ab9da0a4e32baef336e0d2682
Andre Kless <andre.kless@inf.h-brs.de>
2017-10-01 20:54:12 +0200

    add logger configuration for SKS WS17 team building

--- log/resources/configs.js
+++ log/resources/configs.js
@@ -56,5 +56,46 @@ ccm.files[ 'configs.js' ] = {
         }
       }
     }
+  },
+  "sks_ws17_teambuild": {  // created for ccm.log-1.0.0.js, ccm.teambuild-1.0.0.js, ccm.user-2.0.0.js
+    "events": {
+      "ready": {
+        "browser": true,
+        "user": true,
+        "website": true
+      },
+      "start": {
+        "data": true,
+        "user": true
+      },
+      "join": {
+        "data": true,
+        "user": true
+      },
+      "leave": {
+        "data": true,
+        "user": true
+      },
+      "rename": {
+        "data": true,
+        "user": true
+      }
+    },
+    "hash": [ "ccm.load", "https://akless.github.io/ccm-components/libs/md5/md5.min.js" ],
+    "onfinish": {
+      "store_settings": { "store": "sks_ws17_teambuild_log", "url": "https://ccm.inf.h-brs.de" },
+      "permissions": {
+        "creator": "akless2m",
+        "group": {
+          "mkaul2m": true,
+          "akless2m": true
+        },
+        "access": {
+          "get": "group",
+          "set": "creator",
+          "del": "creator"
+        }
+      }
+    }
   }
 };
