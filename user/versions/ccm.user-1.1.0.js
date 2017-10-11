f852b51520dbf1c1df1cdd0ea7d7e684184ea4b1
Andre Kless <andre.kless@inf.h-brs.de>
2017-10-11 13:36:31 +0200

    components uses ccm v11.1.0

--- user/versions/ccm.user-1.1.0.js
+++ user/versions/ccm.user-1.1.0.js
@@ -17,8 +17,8 @@
     version: [ 1, 1, 0 ],
 
     ccm: {
-      url: 'https://akless.github.io/ccm/version/ccm-11.0.0.min.js',
-      integrity: 'sha384-nUXwdn5xynV3fHZa2A1u3X7bC3jCGTW6Da+N2636JNDZXg8eKvfz7Ulr/S0hCu3z',
+      url: 'https://akless.github.io/ccm/version/ccm-11.1.0.min.js',
+      integrity: 'sha384-JLPNKaFhjb0/a3rdhTadUNJB01aw/uBqHFx9+hGrxS8mTG8kQuszsEdnig8++LX4',
       crossorigin: 'anonymous'
     },
 
