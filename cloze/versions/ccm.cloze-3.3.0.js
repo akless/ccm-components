1e66aa7035110c497c03ae2f9150ebb35aa8214c
Andre Kless <andre.kless@inf.h-brs.de>
2017-11-18 14:23:31 +0100

    ccm.cloze.js v3.3.0 - reference text gaps like [[#1]] - reference text gaps with more than one solution word are only correct if each user input is different - bugfix for 'validation' callback - event data contains more informations - outgoing event data is always cloned (except for 'validation' callback) - uses ccm v12.4.0

--- cloze/versions/ccm.cloze-3.3.0.js
+++ cloze/versions/ccm.cloze-3.3.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for rendering a fill-in-the-blank text
  * @author André Kless <andre.kless@web.de> 2017
  * @license The MIT License (MIT)
- * @version latest (3.3.0)
+ * @version 3.3.0
  * @changes
  * version 3.3.0 (18.11.2017):
  * - reference text gaps like [[#1]]
@@ -37,10 +37,6 @@
  * - remove no more needed ccm.helper.protect call
  * - feedback instead of finish when timer has expired
  * version 1.0.0 (12.07.2017)
- * TODO: deactivated finish button activates after submit
- * TODO: docu comments -> API
- * TODO: unit tests
- * TODO: multilingualism
  */
 
 {
@@ -53,10 +49,20 @@
     name: 'cloze',
 
     /**
-     * recommended used framework version
-     * @type {string}
+     * component version
+     * @type {number[]}
      */
-    ccm: 'https://akless.github.io/ccm/ccm.js',
+    version: [ 3, 3, 0 ],
+
+    /**
+     * reference to used framework version
+     * @type {object}
+     */
+    ccm: {
+      url: 'https://akless.github.io/ccm/version/ccm-12.4.0.min.js',
+      integrity: 'sha384-NTSj4YWxeQlG5MFrp2Mi8YUS+yG89M7c4kyruKUdwuYEi2L2KXedjAKLrAuUvG/0',
+      crossorigin: 'anonymous'
+    },
 
     /**
      * default instance configuration
@@ -104,7 +110,7 @@
           "inner": "%%"
         }
       },
-      "css": [ "ccm.load", "../cloze/resources/default.css" ],
+      "css": [ "ccm.load", "https://akless.github.io/ccm-components/cloze/resources/default.css" ],
       "text": "Hello, [[(W)o(rl)d]]!",
       "captions": {
         "start": "Start",
