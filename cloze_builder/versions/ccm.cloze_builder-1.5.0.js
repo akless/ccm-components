c717279b7ecb08f0f1414a9837aea98aa694f8e8
Andre Kless <andre.kless@inf.h-brs.de>
2017-12-06 23:11:22 +0100

    ccm.cloze_builder.js v1.5.0: images could be used inside the text editor and updated help texts

--- cloze_builder/versions/ccm.cloze_builder-1.5.0.js
+++ cloze_builder/versions/ccm.cloze_builder-1.5.0.js
@@ -3,7 +3,7 @@
  * @description This code is based on the ccm component 'ccm.fill_in_the_blank_blank_text_builder-2.0.0.js' by Tea Kless.
  * @author André Kless <andre.kless@web.de>, 2017
  * @license The MIT License (MIT)
- * @version latest (1.5.0)
+ * @version 1.5.0
  * @changes
  * version 1.5.0 (06.12.2017): images could be used inside the text editor and updated help texts
  * version 1.4.0 (27.11.2017): more compact inputs mask (pull request by Tea Kless) and bugfix for restart button
@@ -11,8 +11,6 @@
  * version 1.2.0 (18.11.2017): uses ccm 12.4.0, uses ccm.cloze.js v3.3.0 and updated help text
  * version 1.1.0 (15.11.2017): help texts for input elements
  * version 1.0.0 (13.11.2017)
- * TODO: my.data instead of start_values
- * TODO: helper.fillForm(my.data)
  */
 
 {
@@ -25,10 +23,20 @@
     name: 'cloze_builder',
 
     /**
-     * recommended used framework version
-     * @type {string}
+     * component version
+     * @type {number[]}
      */
-    ccm: 'https://akless.github.io/ccm/ccm.js',
+    version: [ 1, 5, 0 ],
+
+    /**
+     * reference to used framework version
+     * @type {object}
+     */
+    ccm: {
+      url: 'https://akless.github.io/ccm/version/ccm-12.8.0.min.js',
+      integrity: 'sha384-Z9a6XNKaNetgzAttxGs10YlOi0jNjGACggebpAKRvOgV56M1swC2HNJowR89c3gE',
+      crossorigin: 'anonymous'
+    },
 
     /**
      * default instance configuration
@@ -50,7 +58,6 @@
             "class": "form",
             "onsubmit": "%submit%",
             "inner": [
-
               {
                 "class": "text form-group",
                 "inner": [
@@ -634,7 +641,7 @@
         ]
       },
       "css": [ "ccm.load",
-        "../cloze_builder/resources/default.css",
+        "https://akless.github.io/ccm-components/cloze_builder/resources/default.css",
         "https://tkless.github.io/ccm-components/lib/bootstrap/css/bootstrap.css",
         { "context": "head", "url": "https://tkless.github.io/ccm-components/lib/bootstrap/css/font-face.css" }
       ],
@@ -652,7 +659,7 @@
           [ "image" ]
         ], "settings.placeholder": "Type here..." }
       ],
-      "target": [ "ccm.component", "../cloze/ccm.cloze.js" ],
+      "target": [ "ccm.component", "https://akless.github.io/ccm-components/cloze/versions/ccm.cloze-3.4.0.min.js" ],
       "submit_button": true,
       "preview": true,
       "onfinish": { "log": true }
