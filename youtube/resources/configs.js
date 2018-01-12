4c575c00b016d85d96b90abeebbe02a343738ca0
Andre Kless <andre.kless@inf.h-brs.de>
2018-01-12 23:04:45 +0100

    update configurations for ccm.youtube.js

--- youtube/resources/configs.js
+++ youtube/resources/configs.js
@@ -7,14 +7,19 @@
 ccm.files[ 'configs.js' ] = {
   "local": {
     "css": [ "ccm.load", "../youtube/resources/default.css" ],
-    "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
-    "user": [ "ccm.instance", "../user/ccm.user.js" ],
+    "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "youtube" ] ],
+    "user": [ "ccm.instance", "../user/ccm.user.js", { "sign_on": "demo", "logged_in": "true" } ],
     "onfinish": { "restart": true }
   },
   "demo": {
     "css": [ "ccm.load", "https://akless.github.io/ccm-components/youtube/resources/default.css" ],
     "logger": [ "ccm.instance", "https://akless.github.io/ccm-components/log/versions/ccm.log-2.0.0.min.js", [ "ccm.get", "https://akless.github.io/ccm-components/log/resources/configs.min.js", "greedy" ] ],
     "user": [ "ccm.instance", "https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.1.min.js" ],
+    "onfinish": { "restart": true }
+  },
+  "se_ws17": {
+    "logger": [ "ccm.instance", "https://akless.github.io/ccm-components/log/versions/ccm.log-2.0.1.min.js", [ "ccm.get", "https://akless.github.io/ccm-components/log/resources/configs.min.js", "sw_ws17_youtube" ] ],
+    "user": [ "ccm.instance", "https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.1.min.js", { "sign_on": "hbrsinfkaul", "logged_in": true } ],
     "onfinish": { "restart": true }
   }
 };
