9e5765339aef6ab720a6ccec6b0ef81f7d1b26f8
Andre Kless <andre.kless@inf.h-brs.de>
2018-01-15 20:59:11 +0100

    ccm.cloze.js v3.8.0: input fields for gaps are customizable

--- cloze/versions/ccm.cloze-3.8.0.js
+++ cloze/versions/ccm.cloze-3.8.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for rendering a fill-in-the-blank text
  * @author André Kless <andre.kless@web.de> 2017-2018
  * @license The MIT License (MIT)
- * @version latest (3.8.0)
+ * @version 3.8.0
  * @changes
  * version 3.8.0 (15.01.2018): input fields for gaps are customizable
  * version 3.7.0 (09.01.2018):
@@ -50,10 +50,6 @@
  * - remove no more needed ccm.helper.protect call
  * - feedback instead of finish when timer has expired
  * version 1.0.0 (12.07.2017)
- * TODO: deactivated finish button activates after submit
- * TODO: docu comments -> API
- * TODO: unit tests
- * TODO: multilingualism
  */
 
 {
@@ -66,10 +62,20 @@
     name: 'cloze',
 
     /**
-     * recommended used framework version
-     * @type {string}
+     * component version
+     * @type {number[]}
      */
-    ccm: 'https://akless.github.io/ccm/ccm.js',
+    version: [ 3, 8, 0 ],
+
+    /**
+     * reference to used framework version
+     * @type {object}
+     */
+    ccm: {
+      url: 'https://akless.github.io/ccm/version/beta/ccm-14.2.0.min.js',
+      integrity: 'sha384-XimgtmN7p/obnOYclkxk6asSBE1sqwVoC+DWzU1Md0+zhppz9XQ+jCMvsw6gWGoj',
+      crossorigin: 'anonymous'
+    },
 
     /**
      * default instance configuration
@@ -135,7 +141,7 @@
           "inner": "%%"
         }
       },
-      "css": [ "ccm.load", "../cloze/resources/default.css" ],
+      "css": [ "ccm.load", "https://akless.github.io/ccm-components/cloze/resources/default.css" ],
       "text": "Hello, [[(W)o(rl)d]]!",
       "captions": {
         "start": "Start",
