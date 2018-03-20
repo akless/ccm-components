ff20f84b42d090613694caf6bc9770352c7cd0bb
Andre Kless <andre.kless@inf.h-brs.de>
2018-03-20 14:14:58 +0100

    no usage of latest components in demo configruation of ccm.eval.js

--- eval/resources/configs.js
+++ eval/resources/configs.js
@@ -8,8 +8,8 @@ ccm.files[ 'configs.js' ] = {
   "demo": {
     "expression": "{\n  \"foo\": \"bar\",\n  \"numbers\": [ 1, 2, 3 ],\n  \"i\": 5711,\n  \"valid\": true\n}",
     "json_parse": true,
-    "user": [ "ccm.instance", "https://akless.github.io/ccm-components/user/ccm.user.js" ],
-    "logger": [ "ccm.instance", "https://akless.github.io/ccm-components/log/ccm.log.js", [ "ccm.get", "https://akless.github.io/ccm-components/log/resources/configs.js", "greedy" ] ]
+    "user": [ "ccm.instance", "https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.1.js" ],
+    "logger": [ "ccm.instance", "https://akless.github.io/ccm-components/log/versions/ccm.log-2.0.1.js", [ "ccm.get", "https://akless.github.io/ccm-components/log/resources/configs.js", "greedy" ] ]
   },
   "local": {
     "expression": "{\n  \"foo\": \"bar\",\n  \"numbers\": [ 1, 2, 3 ],\n  \"i\": 5711,\n  \"valid\": true\n}",
